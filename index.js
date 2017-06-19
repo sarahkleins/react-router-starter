b81692508ae00b47e78f362167cbdafd549cf8d6
sarahkleins <kleinman.sarah@gmail.com>
2017-06-19 13:23:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import Main from './pages/main.js';
+const history = createHistory();
+
+const App = () => (
+  <Router history={ history }>
+    <div>
+      <Route exact path='/' component={ Main } />
+    </div>
+  </Router>
+);
+
+ReactDOM.render((
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('app'));
---
+++ test/main.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+import React from 'react';
+import Main from '../pages/main';
+import { mount } from 'enzyme';
+
+test('welcomes the user to React Router', function() {
+  const wrapper = mount(<Main />);
+
+  expect(wrapper.text()).toContain('Hello React Router!');
+});
